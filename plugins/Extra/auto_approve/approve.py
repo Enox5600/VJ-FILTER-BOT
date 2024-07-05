fed06c7d244f01d0187bea98257d13b5db678238
Enox5600 <91581852+Enox5600@users.noreply.github.com>
2024-07-05 10:34:48 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -30,10 +30,10 @@ async def auto_approve(client, message: ChatJoinRequest):
             return 
         data = await db.get_msg_command(ap_user_id)
         
-        if data.split("-", 1)[0] == "VJ":
+        if data.split("-", 1)[0] == "OX":
             user_id = int(data.split("-", 1)[1])
-            vj = await referal_add_user(user_id, message.from_user.id)
-            if vj and PREMIUM_AND_REFERAL_MODE == True:
+            OX = await referal_add_user(user_id, message.from_user.id)
+            if OX and PREMIUM_AND_REFERAL_MODE == True:
                 await client.send_message(message.from_user.id, f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
                 num_referrals = await get_referal_users_count(user_id)
                 await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))
